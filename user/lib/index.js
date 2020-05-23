015e56d0594577b369694cf8d12a11061b64e8d3
Yuv <billiondollarone07@gmail.com>
2020-05-24 00:07:15 +0530

    Backup commit
    Authentication complete, user page created
    Storage and realtime updates pending

--- user/lib/index.js
+++ user/lib/index.js
@@ -1,15 +1,14 @@
-// Height - width index
-
 var vHeight = $(window).height(), vWidth = $(window).width(), cover = $('.full'), halfvh = vHeight/2, thirdvh = vHeight/3, halfvw = vWidth/2.5;
 cover.css({"min-height":vHeight,"max-width":vWidth,});
-$('.banner').css({"height": $('.navbar').height() + 16,});
-
-$(document).ready(function(){
-    if($(window).width() < 1000) {
-        $(".nav").addClass("flex-column text-center");
-    }
-});
-
+document.documentElement.style.setProperty('--navbar-height', $('.navbar').height() + 16 + "px");
+function closeUsernav(){
+    $('.usernav').addClass('fadeout');
+    setTimeout(function(){
+        $('.usernav').removeClass('show');
+        $('.usernav').removeClass('fadeout');
+        $('body').css('overflow', 'auto');
+    }, 800);
+};
 // Smooth scroll
 //
 //$(document).ready(function (){
@@ -29,24 +28,21 @@ $(document).ready(function(){
 $('.nav-link').each(function(){
     $(this).click(function(){
         $(this).tab("show");
+        closeUsernav();
         $('html, body').delay(400).animate({
             scrollTop: $($(this).attr("href")).offset().top
         }, 1000);
     })
 });
 // Password confirmation
-$('input[type="password"]').on('input', function(){
-    if($('#password').val() != $('#conpassword').val()){
-        $('#conpassword').css({'box-shadow': '0 0 0 0.2rem rgba(255,0,0,.25)'});
-        $("button[type='submit']").prop('disabled', true);
-        $("#passtoggle").removeClass('hide');
-    } else {
-        $("#passtoggle").addClass('hide');
-        $('#conpassword').css({'box-shadow': '0 0 0 0.2rem rgba(0,255,0,.25)'});
-        $("button[type='submit']").prop('disabled', false);
-    }
-});
 $('#passtoggle').on('click', function(){
     $('input[type="password"]').attr('type') ? 
     $('input[type="password').removeAttr('type') : $('input[type="password').attr('type', 'password');
+});
+$('.navbar-toggler').on('click', function(){
+    $('.usernav').addClass('show');
+    $('body').css('overflow', 'hidden');
+});
+$('.closebtn').on('click', function(){
+    closeUsernav();
 });
--- user/lib/user.js
+++ user/lib/user.js
@@ -1,38 +1,58 @@
 (function(){
     const authRoot = firebase.auth();
     const googleAuth = new firebase.auth.GoogleAuthProvider();
+    const dbRoot = firebase.database();
+    const storageRoot = firebase.storage();
+    const storageRootRef = firebase.storage().ref();
     const userOperating = authRoot.currentUser;
     const btnRegis = document.getElementById('submit');
     const warningPop = document.getElementById('warning');
+    const logOutBtn = $('#logOut');
+    const editChangesBtn = $('#editChanges');
     var userName, userMail, userPic;
-    
-    authRoot.onAuthStateChanged(function(user){
-        if(user){
-            console.log(user)
-        }
-    });
-    authRoot.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(function(){
-        console.log("User is logged till sign out.");
-    }).catch(function(e){
+    function refreshPage(){
+        window.location.reload();
+    };
+    function displayWarning() {
         warningPop.innerHTML = e.message;
         if (e.message != '') {
             warningPop.classList.add('show');
         } else {
             warningPop.classList.remove('show');
         }
+    };
+    
+    authRoot.onAuthStateChanged(function(user){
+        if(user){
+            userName = user.displayName;
+            userMail = user.email;
+            userPic = user.photoURL;
+            $('.card-img-top').attr('src', userPic);
+            $('.card-title').attr('value', userName);
+            $('.card-text').attr('value', "<" + userMail + ">");
+            // Update userName
+            if(userName == null) {
+                $('.card-title').addClass('form-control');
+                $('.card-title').prop('readonly', false);
+                editChangesBtn.on('click', function(){
+                    user.updateProfile({
+                        displayName: $('.card-title').val(),
+                    }).then().catch(function (e) {displayWarning()});
+                });
+            };
+            if (userPic == null) {
+                user.updateProfile({
+                    photoURL: "./lib/user.png",
+                }).then().catch(function(e){displayWarning()});
+            };
+            
+            // Log out current session
+            logOutBtn.on('click', function(){
+                authRoot.signOut().then(function(){
+                    window.location.replace('../index.html')
+                }).catch(function (e) {displayWarning()});
+            });
+        }
     });
-    // End
-    // Check for existing user
-    if (userOperating != null) {
-        userName = userOperating.displayName;
-        userMail = userOperating.email;
-        userPic = userOperating.photoURL;
-        console.log(userOperating + " " + userName + " " + userMail);
-        userOperating.providerData.forEach(function (profile) {
-            userName = profile.displayName;
-            userMail = profile.email;
-            userPic = profile.photoURL;
-            console.log(userOperating + " " + userName + " " + userMail);
-        });
-    }
+    authRoot.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(function(){}).catch(function(e){displayWarning()});
 }())
